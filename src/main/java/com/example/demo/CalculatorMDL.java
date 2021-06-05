877561291bd8545745816fc48f1c8ecad6aa34e2
PotatOceanus <39735195+PotatOceanus@users.noreply.github.com>
2021-06-05 23:37:16 +1000

    Initial commit

--- src/main/java/com/example/demo/CalculatorMDL.java
+++ src/main/java/com/example/demo/CalculatorMDL.java
@@ -11,16 +11,20 @@ import java.util.List;
 @Controller
 public class CalculatorMDL {
 
-    @ResponseBody
-    @GetMapping("CalculatorMDL")
-    public List getData(@RequestParam(value = "Max") int max,
-                        @RequestParam(value = "Divisor") int divisor,
-                        @RequestParam(value = "Limit") int limit) {
+    static List getNumbers (int max, int divisor, int limit) {
         List<String> numbers = new ArrayList<>();
         int start = max - max % divisor + 2;
         for (int i = 0; i < limit; i++) {
             numbers.add("I am " + String.valueOf(start - divisor * i));
         }
         return numbers;
+    }
+
+    @ResponseBody
+    @GetMapping("CalculatorMDL")
+    public List getData(@RequestParam(value = "Max") int max,
+                        @RequestParam(value = "Divisor") int divisor,
+                        @RequestParam(value = "Limit") int limit) {
+        return getNumbers(max, divisor, limit);
     }
 }
